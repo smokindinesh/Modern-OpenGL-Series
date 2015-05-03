770f2904cbdc6c419f97c3fa98bbff95de350ebd
smokin <smokindinesh@gmail.com>
2015-05-03 14:42:34 +0545

    5 Perspective Projection Added

--- 5_PerspectiveProjection/main.cpp
+++ 5_PerspectiveProjection/main.cpp
@@ -4,6 +4,9 @@
 #include <GL/glew.h>
 #include <GL/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+//#include <iostream>
 
 #include <cassert>
 #include <iostream>
@@ -15,7 +18,35 @@ GLuint gVAO = 0;
 GLuint gVBO = 0;
 GLuint programId;
 GLFWwindow* MainWindow = NULL;
-const glm::vec2 SCREEN_SIZE(800, 600);
+glm::vec2 SCREEN_SIZE(800, 600);
+
+glm::mat4 projectionMatrix; // Store the projection matrix
+glm::mat4 viewMatrix; // Store the view matrix
+glm::mat4 modelMatrix; // Store the model matrix
+
+//Called when a key is pressed
+void handleKeypress(GLFWwindow* window,int key,int scancode, int action, int mods) //The key that was pressed
+{
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        exit(0);
+}
+
+//Called when the window is resized
+void handleResize(GLFWwindow* window,int width,int height)
+{
+    //Tell OpenGL how to convert from coordinates to pixel values
+    glViewport( 0, 0, width, height );
+    SCREEN_SIZE= glm::vec2(width,height);
+
+
+    glMatrixMode( GL_PROJECTION ); //Switch to setting the camera perspective
+    //Set the camera perspective
+    glLoadIdentity(); //reset the camera
+                         //The far z clipping coordinate
+}
+
+
+
 GLuint loadShader(char *shaderFile, GLenum type)
 {
     std::ifstream in(shaderFile);
@@ -25,7 +56,7 @@ GLuint loadShader(char *shaderFile, GLenum type)
     src += line + "\n";
     std::cout << src;
     GLuint shader;
-//    GLint compiled;
+    GLint compiled;
     shader = glCreateShader(type);
 
     const char* source = src.c_str();
@@ -67,6 +98,28 @@ GLuint createShaderProgram()
     }
     glUseProgram(programId);
 
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    projectionMatrix = glm::perspective(45.0f, (float)SCREEN_SIZE.x / (float)SCREEN_SIZE.y, 0.1f, 100.0f);
+    // Camera matrix
+    viewMatrix       = glm::lookAt(
+    glm::vec3(0,0,3), // Camera is at (4,3,3), in World Space
+    glm::vec3(0,0,0), // and looks at the origin
+    glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );
+    // Model matrix : an identity matrix (model will be at the origin)
+    modelMatrix      = glm::mat4(1.0f);  // Changes for each model !
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 MVP        = projectionMatrix * viewMatrix * modelMatrix; // Remember, matrix multiplication is the other way around
+    // Get a handle for our "MVP" uniform.
+    // Only at initialisation time.
+    GLuint MatrixID = glGetUniformLocation(programId, "MVP");
+
+    // Send our transformation to the currently bound shader,
+    // in the "MVP" uniform
+    // For each model you render, since the MVP will be different (at least the M part)
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+
  return programId;
 }
 
@@ -82,31 +135,24 @@ static void LoadTriangle() {
     // Put the three triangle verticies into the VBO
     GLfloat vertexData[] = {
         //  X     Y     Z
-         //Pentagon
-        0.25f, 0.25f, 1.0f,
-        0.75f, 0.25f, 1.0f,
-        0.25f, 0.5f, 1.0f,
 
-        0.25f, 0.5f, 1.0f,
-        0.75f, 0.25f, 1.0f,
-        0.75f, 0.5f, 1.0f,
+        //Pentagon
+        0.5f, 0.5f, -5.0f,
+        1.5f, 0.5f, -5.0f,
+        0.5f, 1.0f, -5.0f,
 
-        0.25f, 0.5f, 1.0f,
-        0.75f, 0.5f, 1.0f,
-        0.5f, .75f, 1.0f,
+        0.5f, 1.0f, -5.0f,
+        1.5f, 0.5f, -5.0f,
+        1.5f, 1.0f, -5.0f,
+
+        0.5f, 1.0f, -5.0f,
+        1.5f, 1.0f, -5.0f,
+        1.0f, 1.5f, -5.0f,
 
         //Triangle
-        -0.25f, 0.25f, 1.0f,
-        -0.5f, 0.75f, 1.0f,
-        -0.75f, 0.25f, 1.0f,
-        //Trapezoid
-        -0.2f, -0.25f, 1.0f,
-        -0.35f, -0.75f, 1.0f,
-        0.35f, -0.75f, 1.0f,
-
-        0.2f, -0.25f, 1.0f,
-        -0.2f, -0.25f, 1.0f,
-        0.35f, -0.75f, 1.0f,
+        -0.5f, 0.5f, -5.0f,
+        -1.0f, 1.5f, -5.0f,
+        -1.5f, 0.5f, -5.0f,
     };
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
 
@@ -120,18 +166,22 @@ static void LoadTriangle() {
 }
 
 // draws a single frame
-static void Render(GLFWwindow* MainWindow) {
+static void Render(GLFWwindow* window) {
     // clear everything
     glClearColor(0, 0, 0, 1); // black
     glClear(GL_COLOR_BUFFER_BIT);
+
     // bind the VAO (the triangle)
     glBindVertexArray(gVAO);
+
     // draw the VAO
-    glDrawArrays(GL_TRIANGLES, 0, 18);
+    glDrawArrays(GL_TRIANGLES, 0, 12);
+
     // unbind the VAO
     glBindVertexArray(0);
+
     // swap the display buffers (displays what was just drawn)
-    glfwSwapBuffers(MainWindow);
+    glfwSwapBuffers(window);
 }
 
 // the program starts here
@@ -146,11 +196,13 @@ void MainAppFunc() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-    MainWindow = glfwCreateWindow((int)SCREEN_SIZE.x, (int)SCREEN_SIZE.y,"Draw Basic Shapes",NULL,NULL);
+    MainWindow = glfwCreateWindow((int)SCREEN_SIZE.x, (int)SCREEN_SIZE.y,"Perspective Projection",NULL,NULL);
     if(!MainWindow)
         throw std::runtime_error("glfwOpenWindow failed. Can your hardware handle OpenGL 4.2?");
 
-    // GLFW settings
+    glfwSetWindowSizeCallback(MainWindow,handleResize); //callback function of GLFW to handle window resize
+    glfwSetKeyCallback(MainWindow,handleKeypress); //callback function to handle keypress
+
     glfwMakeContextCurrent(MainWindow);
     // initialise GLEW
     glewExperimental = GL_TRUE; //stops glew crashing on OSX :-/
