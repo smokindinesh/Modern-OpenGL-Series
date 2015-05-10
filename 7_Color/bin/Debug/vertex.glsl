2b243a1c2c045021518b207d311c91f78083e9c6
smokin <smokindinesh@gmail.com>
2015-05-10 10:53:20 +0545

    7 Color Added

---
+++ 7_Color/bin/Debug/vertex.glsl
@@ -0,0 +1,13 @@
+#version 150
+
+uniform mat4 model;
+uniform mat4 prespective;
+uniform mat4 camera;
+in vec3 vPosition;
+in vec4 vColor;
+out vec4 outColor;
+void main() {
+    // does not alter the verticies at all
+    gl_Position = prespective*camera*model*vec4(vPosition, 1);
+    outColor=vColor;
+}
---
+++ 7_Color/fragment.glsl
@@ -0,0 +1,10 @@
+#version 420
+
+in vec4 outColor;
+out vec4 finalColor;
+
+void main() {
+    //set every drawn pixel to white
+    //finalColor = vec4(1.0, 1.0, 1.0, 1.0);
+    finalColor = outColor;
+}
